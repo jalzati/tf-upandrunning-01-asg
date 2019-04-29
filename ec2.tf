addf5a6ffd10f00df5e2b4e8fa31b158f48e0e43
Jorge Alzati <alzati.jorge@gmail.com>
2019-04-28 19:51:46 -0700

    Checking in the ASG code

--- ec2.tf
+++ ec2.tf
@@ -32,9 +32,56 @@ resource "aws_security_group" "sg-webserver" {
     Department = "DevOps"
     Environment = "research"
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_key_pair" "webserver_keypair" {
   key_name   = "${var.key_name}"
   public_key = "${file(var.public_key_path)}"
+}
+
+resource "aws_launch_configuration" "webserver_launch_config" {
+  image_id        = "${var.ami}"
+  instance_type   = "${var.instance_type}"
+  security_groups = ["${aws_security_group.sg-webserver.id}"]
+
+  user_data = "${file("install_apache.sh")}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "webserver_asg" {
+  launch_configuration = "${aws_launch_configuration.webserver_launch_config.id}"
+
+  min_size = 2
+  max_size = 4
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key = "Owner"
+    propagate_at_launch = false
+    value = "${var.owner}"
+  }
+
+  tag {
+    key = "Deparment"
+    propagate_at_launch = false
+    value = "${var.department}"
+  }
+
+  tag {
+    key = "Environment"
+    propagate_at_launch = false
+    value = "${var.environment}"
+  }
 }
