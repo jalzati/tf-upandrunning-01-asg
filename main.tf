969849d0bed1f7b59a3959fe48ebdb8d0ed2b06e
Jorge Alzati <alzati.jorge@gmail.com>
2019-05-12 20:43:45 -0700

    Adding code to store tfstate file on a s3 bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "${var.region}"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "anomali-alexa-terraform-state"
+    key = "tf-upandrunning-01-asg-tfstate"
+    region = "ca-central-1"
+  }
+}
+
